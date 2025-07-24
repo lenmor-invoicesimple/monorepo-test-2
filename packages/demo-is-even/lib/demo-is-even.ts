1ee1596195076ac791b2c169430bf34eeaa2eec6
Lenmor Dimanalata <lenmor@invoicesimple.com>
2025-07-24 14:05:19 -0400

    link demo-even to demo-odd

--- packages/demo-is-even/lib/demo-is-even.ts
+++ packages/demo-is-even/lib/demo-is-even.ts
@@ -1,4 +1,4 @@
-import { isOdd } from "demo-is-odd";
+import { isOdd } from "@lenmor-invoicesimple/demo-is-odd-2";
 
 export function isEven(num: number): boolean {
   console.log("isEven called with 111:", num);
